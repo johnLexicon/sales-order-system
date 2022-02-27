fe4ebc6aba3797bb8de539256ccc07e6e08567f8
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-02-27 20:47:48 +0100

    Added export in barrel.

--- models/index.mjs
+++ models/index.mjs
@@ -3,6 +3,7 @@ export * from './cash.mjs';
 export * from './contact.mjs';
 export * from './credit.mjs';
 export * from './customer.mjs';
+export * from './item.mjs';
 export * from './order.mjs';
 export * from './orderDetail.mjs';
 export * from './orderStatus.mjs';
